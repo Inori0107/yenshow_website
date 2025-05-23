413607c1163807e4af607cfc9c52d5aec8c5c252
InoriLin0107 <InoriLin0107@gmail.com>
2025-05-23 09:15:09 +0800

    GSAP 調整

--- components/products/SkeletonIntroduction.vue
+++ components/products/SkeletonIntroduction.vue
@@ -1,9 +1,8 @@
 <template>
-	<div class="flex flex-col md:flex-row justify-center gap-4 animate-pulse">
-		<div v-for="n in 4" :key="n" class="bg-white border border-gray-200 rounded-lg shadow-md p-4 w-[250px]">
-			<div class="h-6 bg-gray-300 rounded mb-3"></div>
-			<div class="h-4 bg-gray-300 rounded mb-1"></div>
-			<div class="h-4 bg-gray-300 rounded w-5/6"></div>
+	<div class="grid grid-cols-1 md:grid-cols-2 gap-3 sm:gap-4 animate-pulse w-full">
+		<div v-for="n in 4" :key="n" class="bg-slate-50 border border-slate-200 rounded-lg shadow-md p-3 sm:p-4 text-center h-[100px] sm:h-[120px]">
+			<div class="h-5 sm:h-6 bg-gray-300 rounded mb-2 sm:mb-3 w-3/4 mx-auto"></div>
+			<div class="h-3 sm:h-4 bg-gray-300 rounded w-5/6 mx-auto"></div>
 		</div>
 	</div>
 </template>
